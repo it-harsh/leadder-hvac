c4fccfef02d478588e31d8877679302fcc8b1b33
adamkm111 <adamkmaltais@gmail.com>
2026-06-15 14:31:09 +0530

    feat(email): add mailtrap client

---
+++ lib/email/mailtrap.ts
@@ -0,0 +1,15 @@
+import { MailtrapClient } from 'mailtrap'
+
+const useSandbox = !!process.env.MAILTRAP_TEST_INBOX_ID
+
+export const mailtrap = new MailtrapClient({
+  token: process.env.MAILTRAP_API_TOKEN!,
+  ...(useSandbox
+    ? { sandbox: true, testInboxId: Number(process.env.MAILTRAP_TEST_INBOX_ID) }
+    : {}),
+})
+
+export const FROM = {
+  email: process.env.MAILTRAP_FROM_EMAIL ?? 'noreply@leadder.io',
+  name: process.env.MAILTRAP_FROM_NAME ?? 'Leadder',
+}
